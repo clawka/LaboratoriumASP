1cf17ec15f12b8afdfdf957b78b5f0f97b92a9d0
stsiapan.yakaukin <stsiapan.yakaukin@microsoft.wsei.edu.pl>
2024-10-15 17:58:43 +0200

    Maksznakses

---
+++ WebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: CalculatorController.cs
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Result(int x,string op,int y)
+        {
+      
+            switch (op)
+            {
+                case "+":
+                    ViewBag.Result = x+y;
+                    break;
+                case "-":
+                    ViewBag.Result = x-y;
+                    break;
+                case "*":
+                    ViewBag.Result = x*y;
+                    break;
+                case "/":
+                    ViewBag.Result = x/y;
+                    break;
+                default:
+                    ViewBag.Result = "b";
+                    break;
+            }
+            return View();
+        }
+        public IActionResult Form(int x,string op,int y)
+        {
+            return Result(x,op,y);
+        }
+    }
+}
