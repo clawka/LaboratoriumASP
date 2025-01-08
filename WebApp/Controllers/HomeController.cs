92239587806b7cbcbb924cb03880371eeef018ec
Stsiapan Yakaukin <stepa.yakovkin@gmail.com>
2025-01-08 21:30:54 +0100

    solution lab 1

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -4,6 +4,11 @@ using WebApp.Models;
 
 namespace WebApp.Controllers;
 
+public enum Operator
+{
+    Unknown, Add, Mul, Sub, Div
+}
+
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
@@ -12,7 +17,55 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
+    
+    public IActionResult About()
+    {
+        return View();
+    }
 
+    public IActionResult Calculator(Operator? op, double? a, double? b)
+    {
+        if (a is null || b is null)
+        {
+            ViewBag.errorMessage = "Niepoprawny format liczby w parametrze a lub b";
+            return View("ErrorMessage");
+        }
+        if (op is not Operator)
+        {
+            ViewBag.errorMessage = "Nieznany operator";
+            return View("ErrorMessage");
+        }
+
+        ViewBag.a = a;
+        ViewBag.b = b;
+        
+        switch(op)
+        {
+            case Operator.Add:
+                ViewBag.Result = a + b;
+                ViewBag.op = "+";
+                break;
+            case Operator.Sub:
+                ViewBag.Result = a - b;
+                ViewBag.op = "-";
+                break;
+            case Operator.Mul:
+                ViewBag.Result = a * b;
+                ViewBag.op = "*";
+                break;
+            case Operator.Div:
+                if (b == 0)
+                {
+                    ViewBag.errorMessage = "Nie mozna dzielic na 0";
+                    return View("ErrorMessage");
+                }
+                ViewBag.Result = a / b;
+                ViewBag.op = "/";
+                break;
+        }
+        return View();
+    }
+    
     public IActionResult Index()
     {
         return View();
---
+++ WebApp/Views/Home/Calculator.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Views.Home;
+
+public class Calculator : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
